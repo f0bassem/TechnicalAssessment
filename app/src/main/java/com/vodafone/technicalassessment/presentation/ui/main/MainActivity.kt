5f7b123b1a699e2d6e1b0b7c121999e64e102e70
fady0bassem <fady0bassem@gmail.com>
2022-01-10 04:39:46 +0200

    check network status

--- app/src/main/java/com/vodafone/technicalassessment/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/vodafone/technicalassessment/presentation/ui/main/MainActivity.kt
@@ -7,27 +7,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.os.bundleOf
 import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.ads.MobileAds
 import com.vodafone.technicalassessment.R
 import com.vodafone.technicalassessment.domain.dto.main.ListItemDTO
+import com.vodafone.technicalassessment.presentation.network_state.ConnectivityStatus
 import com.vodafone.technicalassessment.presentation.ui.details.DetailsActivity
 import com.vodafone.technicalassessment.presentation.ui.theme.TechnicalAssessmentTheme
 import com.vodafone.technicalassessment.utils.Constants.Companion.ITEM
@@ -58,62 +54,72 @@ class MainActivity : ComponentActivity() {
                     scaffoldState = scaffoldState
                 ) {
                     Surface(color = MaterialTheme.colors.background) {
-
-                        // empty view
-                        Column(
-                            modifier = Modifier.fillMaxSize(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            AnimatedVisibility(
-                                modifier = Modifier,
-                                visible = itemsList.size == 0 && apiStatus.value != Status.LOADING
+                        Box {
+                            // listing items
+                            LazyColumn(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                state = lazyState
                             ) {
-                                // empty text
-                                Text(
-                                    modifier = Modifier,
-                                    text = stringResource(id = R.string.no_data_available),
-                                    color = Color.LightGray,
-                                    style = MaterialTheme.typography.h6,
-                                    maxLines = 1,
-                                    overflow = TextOverflow.Ellipsis
-                                )
+                                itemsIndexed(
+                                    items = itemsList,
+                                    key = { index, item -> item.id!! }) { index, item ->
 
-                            }
-                        }
-                        
-                        // listing items
-                        LazyColumn(modifier = Modifier.fillMaxSize(), state = lazyState) {
-                            itemsIndexed(
-                                items = itemsList,
-                                key = { index, item -> item.id!! }) { index, item ->
-
-                                // paginate
-                                if (index > itemsList.size - 2) {
-                                    viewModel.page = viewModel.page + 1
-                                    viewModel.onTriggerEvent(MainEvent.GetList)
-                                }
-
-                                // item view
-                                ItemView(
-                                    author = item.author!!,
-                                    image = item.download_url!!,
-                                    showAd = index % 5 == 0 && index > 0,
-                                    onClick = {
-                                        item.rgb = it
-                                        lifecycleScope.launch {
-                                            navigate(item)
-                                        }
+                                    // paginate
+                                    if (index > itemsList.size - 2) {
+                                        viewModel.page = viewModel.page + 1
+                                        viewModel.onTriggerEvent(MainEvent.GetList)
                                     }
-                                )
+
+                                    // item view
+                                    ItemView(
+                                        author = item.author!!,
+                                        image = item.download_url!!,
+                                        showAd = index % 5 == 0 && index > 0,
+                                        onClick = {
+                                            item.rgb = it
+                                            lifecycleScope.launch {
+                                                navigate(item)
+                                            }
+                                        }
+                                    )
+                                }
                             }
+
+                            Column(
+                                modifier = Modifier.fillMaxSize(),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                // empty view
+                                Column(
+                                    modifier = Modifier.fillMaxSize(),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ) {
+                                    AnimatedVisibility(
+                                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                                        visible = itemsList.size == 0 && apiStatus.value != Status.LOADING
+                                    ) {
+                                        // empty text
+                                        Text(
+                                            modifier = Modifier,
+                                            text = stringResource(id = R.string.no_data_available),
+                                            color = Color.LightGray,
+                                            style = MaterialTheme.typography.h6,
+                                        )
+                                    }
+                                }
+                            }
+
+                            // check connection
+                            ConnectivityStatus()
                         }
                     }
                 }
             }
         }
     }
-
 
     private fun navigate(item: ListItemDTO) {
         val intent = Intent(this, DetailsActivity::class.java)
